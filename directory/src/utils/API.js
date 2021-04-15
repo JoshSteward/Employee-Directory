e48754c58f5f95e234a1c08b5ff8596e3856704e
Josh Steward <stewardjosh10@gmail.com>
2021-04-15 11:40:24 +1000

    footer and Navbar added

--- directory/src/utils/API.js
+++ directory/src/utils/API.js
@@ -3,8 +3,5 @@ import axios from "axios";
 export default {
     getEmployees: function() {
         return axios.get("https://randomuser.me/api/?results=50");
-    },
-    getGender: function() {
-        return axios.get("https://randomuser.me/api/?gender=" + gender);
     }
 };
